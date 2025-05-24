2c1b0e8df10efd2f1b1c78c4557b3d86ceceb935
shailjayadav30 <shailjayadav7275@gmail.com>
2025-05-24 16:35:53 +0530

    changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors"
-import contactRoute from "./routes/contactroute"
-import ticketRoute from "./routes/ticketroute"
+import cors from "cors";
+import contactRoute from "./routes/contactroute";
+import ticketRoute from "./routes/ticketroute";
 dotenv.config();
 
 const app = express();
-app.use(express.json());  
-app.use(cors({
-  origin:["http://localhost:5173","https://cricket-website-tan.vercel.app"],
-  credentials:true
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://cricket-website-tan.vercel.app"],
+    credentials: true,
+  })
+);
 const port = process.env.PORT || 5000;
 const mongoUrl = process.env.MONGO_URL;
 
@@ -19,16 +21,15 @@ if (!mongoUrl) {
   console.error("Mongo url is not defined");
   process.exit(1);
 }
-app.get("/",(req,res)=>{
-  res.send("hello")
-})
-app.use("/api",contactRoute)
-app.use("/booking",ticketRoute)
-
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.use("/api", contactRoute);
+app.use("/booking", ticketRoute);
 
 mongoose
   .connect(mongoUrl)
   .then(() => {
     app.listen(port, () => console.log(`Server is running on port ${port}`));
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err,"Failed to connect to db")) 
