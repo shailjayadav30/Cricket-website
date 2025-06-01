cc6b3d4eacd293da4805782ae5fdfe8c2ce7cd4e
shailjayadav30 <shailjayadav7275@gmail.com>
2025-06-01 21:59:32 +0530

    fixed error

--- backend/src/controllers/subscribecontroller.ts
+++ backend/src/controllers/subscribecontroller.ts
@@ -2,12 +2,12 @@ import Subscriber from "../models/subscriber"
 import { Request,Response } from "express"
 export  const subscribe =async(req:Request,res:Response)=>{
     const {email}=req.body
-    if(!email || !email.includes("@")){
-        res.status(400).json({
-            message:"Invalid email"
-        })
-    }
+   
    try {
+     if(!email || !email.includes("@")){
+         res.status(400).json({message:"Invalid email"})
+         return
+    }
     const existing= await Subscriber.findOne({email})
     if(existing){
         res.status(409).json({
